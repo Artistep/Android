753d72e9200e736101745e0ecd2060005f6c2350
김민규 <mingyu5675@gmail.com>
2022-08-31 21:17:03 +0900

    feat: page2 ui 적용중

--- app/src/main/java/com/artistep/artistep/MainActivity.kt
+++ app/src/main/java/com/artistep/artistep/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.artistep.artistep
 
-import com.artistep.artistep.config.BaseActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.artistep.artistep.databinding.ActivityMainBinding
+import com.artistep.artistep.page2.ZoomFragment
 
-class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+    private var container = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
 
+        container = binding.mainContainer.id
+
+        val bottomBar = binding.bottomNav
+        supportFragmentManager.beginTransaction().replace(container, ZoomFragment()).commitAllowingStateLoss()
+
+        bottomBar.setOnItemSelectedListener {
+            getFragment(it)
+        }
+        bottomBar.setOnItemReselectedListener {
+            Log.d("Main","menu reselected")
+        }
+    }
+
+    private fun getFragment(menuItem: MenuItem): Boolean {
+        when(menuItem.itemId){
+            R.id.bottom_bar_film -> {
+                supportFragmentManager.beginTransaction().replace(container, ZoomFragment()).commitAllowingStateLoss()
+            }
+            else -> {
+                supportFragmentManager.beginTransaction().replace(container, ZoomFragment()).commitAllowingStateLoss()
+            }
+        }
+        return true
     }
 }
